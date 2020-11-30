72b795bf63abea4a079f8ffaef49a09dfd5645be
Kritsanaphon Thairueakham <flog9731@hotmail.com>
2020-11-30 09:34:06 +0700

    update byte

---
+++ Get.py
@@ -0,0 +1,11 @@
+import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("webcode.me" , 80))
+    s.sendall(b"GET / HTTP/1.1\r\nHost: webcode.me\r\nAccept: text/html\r\nConnection: close\r\n\r\n")
+    while True:
+        data = s.recv(300)
+        if not data:
+            break
+        print(data.decode())
+        
